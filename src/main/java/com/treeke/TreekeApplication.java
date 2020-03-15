3bea77bcadcc1dd1b7f2668a7b4fa2d817967382
treeke <pickyourself@163.com>
2020-03-15 18:28:00 +0800

    first commit

---
+++ src/main/java/com/treeke/TreekeApplication.java
@@ -0,0 +1,13 @@
+package com.treeke.treeke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreekeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreekeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/treeke/treeke/TreekeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treeke.treeke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreekeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
